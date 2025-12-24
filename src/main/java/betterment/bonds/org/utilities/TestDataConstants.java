e68b6febbde70913602cbefdbaf6c54827035fe3
Amit Dharmale <95748834+dhaamit@users.noreply.github.com>
2025-12-24 17:42:44 -0500

    Delete src directory

--- src/main/java/betterment/bonds/org/utilities/TestDataConstants.java
+++
@@ -1,8 +0,0 @@
-//Author - @Amit Dharmale
-package betterment.bonds.org.utilities;
-
-public class TestDataConstants {
-    /* Constant for Mutual Funds URL */
-    public static final String BONDSURL = "/bonds-investing";
-	public static final String BONDSINVESTINGURL = "https://www.betterment.com/bonds-investing";
-}
